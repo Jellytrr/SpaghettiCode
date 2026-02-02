358e14d99b8cca561d16e3aa79fe8a61efebd049
PortofolioYogish <yogishaditya08@gmail.com>
2026-02-02 07:34:01 +0700

    Add comments on method reusability and code structure
    Add comments explaining the benefits of using methods for reusability and maintainability.

--- index.js
+++ index.js
@@ -20,3 +20,8 @@ const enemy = new Karakter("Dragon", 2000, 50);
 
 player.serang(enemy);
 // Output: Arthur menyerang Dragon, Sisa HP Dragon: 1850
+
+
+// Jawaban Nomor 4 : Dengan menggunakan method diatas, code bisa dibuat reuseable karena satu method bisa dipakai di semua karakter tanpa perlu menulis ulang atau copy paste
+// Jawaban Nomor 4 : Dengan menggunakan method diatas, kode juga jadi lebih rapih dan terstruktur karena setiap logic menempel ke objek yang sama
+// Jawaban Nomor 4 : Dengan menggunakan method diatas, kode juga jadi lebih mudah dikembangkan karena jika ada perubahan logic, kita hanya perlu mengubah di satu tempat saja yaitu di method tersebut
